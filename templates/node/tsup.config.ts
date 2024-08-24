5e924e9c129b1427eb35ddbac2e39d95a4469987
Myoschen <myos.chen@gmail.com>
2024-08-24 12:29:22 +0800

    feat: node template

---
+++ templates/node/tsup.config.ts
@@ -0,0 +1,19 @@
+import { cp } from 'node:fs/promises'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  clean: true,
+  shims: true,
+  outDir: 'dist',
+  onSuccess: async () => {
+    await cp(
+      path.join(path.dirname(fileURLToPath(import.meta.url)), 'templates'),
+      path.join('dist', 'templates'),
+      { recursive: true },
+    )
+  },
+})
