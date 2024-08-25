3baa809527a03740de06e69000e59a77dfe23c4a
Myoschen <myos.chen@gmail.com>
2024-08-25 21:09:42 +0800

    build: update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  minify: true,
   clean: true,
   shims: true,
   outDir: 'dist',
